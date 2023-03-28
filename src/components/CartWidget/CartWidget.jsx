6d6c6e7130cff448435d625c3e438be7a64a7994
Sebastian Rondon <55625968+MrJu4an@users.noreply.github.com>
2023-03-27 22:37:32 -0500

    Ajustes NavBar

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,8 +4,8 @@ import './CartWidget.css';
 export const CardWidget = () => {
     return (
         <>
+            <p className='fs-5'><b className='count'>0</b></p>
             <img className="cart" src={cart} alt="cart-widget"></img>
-            <b className='count'>0</b>
         </>
             
     );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,33 +4,40 @@ import { CardWidget } from '../CartWidget/CartWidget';
 import logo from './assets/logo-idevs.png'
 export const Navbar = () => {
     return(
-        <>
-            <nav className="navbar navbar-dark bg-dark">
-             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img className='logo' src={logo} alt=''></img>
-                    iDevS
-                </a>
-                <ul>
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Procesadores</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Tarjetas de Video</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Teclados</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Blog</a>
-                    </li>
-                </ul>
-                <ul>
-                    <CardWidget />
-                </ul>
-                
+            <div className="container-fluid">
+                <div className='row'>
+                    <nav className="navbar navbar-dark bg-dark">
+                        <div className="container-fluid">
+                            <div className='col-2'>
+                                <a className="navbar-brand" href="#">
+                                    <img className='m-2 logo' src={logo} alt=''></img>
+                                    iDevS
+                                </a>
+                            </div>
+                            <div className='col-8'>
+                                <ul>
+                                    <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Procesadores</a>
+                                    </li>
+                                    <li className="nav-item">
+                                    <a className="nav-link" href="#">Tarjetas de Video</a>
+                                    </li>
+                                    <li className="nav-item">
+                                    <a className="nav-link" href="#">Teclados</a>
+                                    </li>
+                                    <li className="nav-item">
+                                    <a className="nav-link" href="#">Blog</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className='col-2'>
+                                <ul>
+                                <CardWidget />
+                                </ul>
+                            </div>                    
+                        </div>
+                    </nav>
                 </div>
-            </nav>
-        </>
+            </div>
     )
 }
